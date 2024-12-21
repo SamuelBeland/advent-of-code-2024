d6849babfaa63ed97b0c473e5e9d80db10fdd73a
Samuel Béland <samuel.beland@gmail.com>
2024-12-21 18:21:20 -0500

    finished day-1-1

--- src/days/src/day1.cpp
+++ src/days/src/day1.cpp
@@ -3,14 +3,83 @@
 #include <adventlib/Parser.hpp>
 #include <adventlib/utilities.hpp>
 
+#include <numeric>
+
 namespace
 {
+	using Id = std::int32_t;
+	using IdDiff = Id;
 
-}
+	struct IdPair
+	{
+		Id left;
+		Id right;
+	};
 
-std::string sb::day_1_1(std::string const& /*input*/)
+	IdPair parse_id_pair(std::string_view str)
+	{
+		::IdPair result;
+		sb::scan(str, "{}   {}", result.left, result.right);
+		return result;
+	}
+
+	std::vector<IdPair> parse_input(std::string_view str)
+	{
+		return sb::transform(sb::split_lines(str), parse_id_pair);
+	}
+
+	struct IdLists
+	{
+		std::vector<Id> left;
+		std::vector<Id> right;
+	};
+
+	IdLists split_id_lists(std::vector<IdPair> const& id_pairs)
+	{
+		IdLists id_lists;
+		id_lists.left.resize(id_pairs.size());
+		id_lists.right.resize(id_pairs.size());
+
+		for (std::size_t i = 0; i < id_pairs.size(); ++i)
+		{
+			id_lists.left[i] = id_pairs[i].left;
+			id_lists.right[i] = id_pairs[i].right;
+		}
+
+		return id_lists;
+	}
+
+	IdLists sort_id_lists(IdLists id_lists)
+	{
+		std::sort(id_lists.left.begin(), id_lists.left.end());
+		std::sort(id_lists.right.begin(), id_lists.right.end());
+
+		return id_lists;
+	}
+
+	IdDiff get_id_diff_sum(IdLists const& id_lists)
+	{
+		assert(id_lists.left.size() == id_lists.right.size());
+
+		IdDiff sum = 0;
+
+		for (std::size_t i = 0; i < id_lists.left.size(); ++i)
+		{
+			sum += std::abs(id_lists.left[i] - id_lists.right[i]);
+		}
+
+		return sum;
+	}
+} // namespace
+
+std::string sb::day_1_1(std::string const& input)
 {
-	return "";
+	std::vector<IdPair> const id_pairs = parse_input(input);
+	IdLists id_lists = split_id_lists(id_pairs);
+	id_lists = sort_id_lists(std::move(id_lists));
+	IdDiff const id_diff = get_id_diff_sum(id_lists);
+
+	return sb::to_string(id_diff);
 }
 
 std::string sb::day_1_2(std::string const& /*input*/)
--- test/all_tests.cpp
+++ test/all_tests.cpp
@@ -7,255 +7,255 @@
 
 #define RUN_INDIVIDUAL_BENCHMARKS
 
+//==============================================================================
+TEST_CASE("day1")
+{
+	CHECK(sb::run_day_with_test_input(1, 1, 1) == "11");
+	CHECK(sb::run_day_with_default_input(1, 1) == "2430334");
+
+	// CHECK(sb::run_day_with_test_input(1, 2, 1) == "");
+	// CHECK(sb::run_day_with_default_input(1, 2) == "");
+}
+
 ////==============================================================================
-//TEST_CASE("day1")
-//{
-//	// CHECK(sb::run_day_with_test_input(1, 1, 1) == "");
-//	// CHECK(sb::run_day_with_default_input(1, 1) == "");
-//
-//	// CHECK(sb::run_day_with_test_input(1, 2, 1) == "");
-//	// CHECK(sb::run_day_with_default_input(1, 2) == "");
-//}
-//
-////==============================================================================
-//TEST_CASE("day2")
+// TEST_CASE("day2")
 //{
 //	// CHECK(sb::run_day_with_test_input(2, 1, 1) == "");
 //	// CHECK(sb::run_day_with_default_input(2, 1) == "");
 //
 //	// CHECK(sb::run_day_with_test_input(2, 2, 1) == "");
 //	// CHECK(sb::run_day_with_default_input(2, 2) == "");
-//}
+// }
 //
 ////==============================================================================
-//TEST_CASE("day3")
+// TEST_CASE("day3")
 //{
 //	// CHECK(sb::run_day_with_test_input(3, 1, 1) == "");
 //	// CHECK(sb::run_day_with_default_input(3, 1) == "");
 //
 //	// CHECK(sb::run_day_with_test_input(3, 2, 1) == "");
 //	// CHECK(sb::run_day_with_default_input(3, 2) == "");
-//}
+// }
 //
 ////==============================================================================
-//TEST_CASE("day4")
+// TEST_CASE("day4")
 //{
 //	// CHECK(sb::run_day_with_test_input(4, 1, 1) == "");
 //	// CHECK(sb::run_day_with_default_input(4, 1) == "");
 //
 //	// CHECK(sb::run_day_with_test_input(4, 2, 1) == "");
 //	// CHECK(sb::run_day_with_default_input(4, 2) == "");
-//}
+// }
 //
 ////==============================================================================
-//TEST_CASE("day5")
+// TEST_CASE("day5")
 //{
 //	// CHECK(sb::run_day_with_test_input(5, 1, 1) == "");
 //	// CHECK(sb::run_day_with_default_input(5, 1) == "");
 //
 //	// CHECK(sb::run_day_with_test_input(5, 2, 1) == "");
 //	// CHECK(sb::run_day_with_default_input(5, 2) == "");
-//}
+// }
 //
 ////==============================================================================
-//TEST_CASE("day6")
+// TEST_CASE("day6")
 //{
 //	// CHECK(sb::run_day_with_test_input(6, 1, 1) == "");
 //	// CHECK(sb::run_day_with_default_input(6, 1) == "");
 //
 //	// CHECK(sb::run_day_with_test_input(6, 2, 1) == "");
 //	// CHECK(sb::run_day_with_default_input(6, 2) == "");
-//}
+// }
 //
 ////==============================================================================
-//TEST_CASE("day7")
+// TEST_CASE("day7")
 //{
 //	// CHECK(sb::run_day_with_test_input(7, 1, 1) == "");
 //	// CHECK(sb::run_day_with_default_input(7, 1) == "");
 //
 //	// CHECK(sb::run_day_with_test_input(7, 2, 1) == "");
 //	// CHECK(sb::run_day_with_default_input(7, 2) == "");
-//}
+// }
 //
 ////==============================================================================
-//TEST_CASE("day8")
+// TEST_CASE("day8")
 //{
 //	// CHECK(sb::run_day_with_test_input(8, 1, 1) == "");
 //	// CHECK(sb::run_day_with_default_input(8, 1) == "");
 //
 //	// CHECK(sb::run_day_with_test_input(8, 2, 1) == "");
 //	// CHECK(sb::run_day_with_default_input(8, 2) == "");
-//}
+// }
 //
 ////==============================================================================
-//TEST_CASE("day9")
+// TEST_CASE("day9")
 //{
 //	// CHECK(sb::run_day_with_test_input(9, 1, 1) == "");
 //	// CHECK(sb::run_day_with_default_input(9, 1) == "");
 //
 //	// CHECK(sb::run_day_with_test_input(9, 2, 1) == "");
 //	// CHECK(sb::run_day_with_default_input(9, 2) == "");
-//}
+// }
 //
 ////==============================================================================
-//TEST_CASE("day10")
+// TEST_CASE("day10")
 //{
 //	// CHECK(sb::run_day_with_test_input(10, 1, 1) == "");
 //	// CHECK(sb::run_day_with_default_input(10, 1) == "");
 //
 //	// CHECK(sb::run_day_with_test_input(10, 2, 1) == "");
 //	// CHECK(sb::run_day_with_default_input(10, 2) == "");
-//}
+// }
 //
 ////==============================================================================
-//TEST_CASE("day11")
+// TEST_CASE("day11")
 //{
 //	// CHECK(sb::run_day_with_test_input(11, 1, 1) == "");
 //	// CHECK(sb::run_day_with_default_input(11, 1) == "");
 //
 //	// CHECK(sb::run_day_with_test_input(11, 2, 1) == "");
 //	// CHECK(sb::run_day_with_default_input(11, 2) == "");
-//}
+// }
 //
 ////==============================================================================
-//TEST_CASE("day12")
+// TEST_CASE("day12")
 //{
 //	// CHECK(sb::run_day_with_test_input(12, 1, 1) == "");
 //	// CHECK(sb::run_day_with_default_input(12, 1) == "");
 //
 //	// CHECK(sb::run_day_with_test_input(12, 2, 1) == "");
 //	// CHECK(sb::run_day_with_default_input(12, 2) == "");
-//}
+// }
 //
 ////==============================================================================
-//TEST_CASE("day13")
+// TEST_CASE("day13")
 //{
 //	// CHECK(sb::run_day_with_test_input(13, 1, 1) == "");
 //	// CHECK(sb::run_day_with_default_input(13, 1) == "");
 //
 //	// CHECK(sb::run_day_with_test_input(13, 2, 1) == "");
 //	// CHECK(sb::run_day_with_default_input(13, 2) == "");
-//}
+// }
 //
 ////==============================================================================
-//TEST_CASE("day14")
+// TEST_CASE("day14")
 //{
 //	// CHECK(sb::run_day_with_test_input(14, 1, 1) == "");
 //	// CHECK(sb::run_day_with_default_input(14, 1) == "");
 //
 //	// CHECK(sb::run_day_with_test_input(14, 2, 1) == "");
 //	// CHECK(sb::run_day_with_default_input(14, 2) == "");
-//}
+// }
 //
 ////==============================================================================
-//TEST_CASE("day15")
+// TEST_CASE("day15")
 //{
 //	// CHECK(sb::run_day_with_test_input(15, 1, 1) == "");
 //	// CHECK(sb::run_day_with_default_input(15, 1) == "");
 //
 //	// CHECK(sb::run_day_with_test_input(15, 2, 1) == "");
 //	// CHECK(sb::run_day_with_default_input(15, 2) == "");
-//}
+// }
 //
 ////==============================================================================
-//TEST_CASE("day16")
+// TEST_CASE("day16")
 //{
 //	// CHECK(sb::run_day_with_test_input(16, 1, 1) == "");
 //	// CHECK(sb::run_day_with_default_input(16, 1) == "");
 //
 //	// CHECK(sb::run_day_with_test_input(16, 2, 1) == "");
 //	// CHECK(sb::run_day_with_default_input(16, 2) == "");
-//}
+// }
 //
 ////==============================================================================
-//TEST_CASE("day17")
+// TEST_CASE("day17")
 //{
 //	// CHECK(sb::run_day_with_test_input(17, 1, 1) == "");
 //	// CHECK(sb::run_day_with_default_input(17, 1) == "");
 //
 //	// CHECK(sb::run_day_with_test_input(17, 2, 1) == "");
 //	// CHECK(sb::run_day_with_default_input(17, 2) == "");
-//}
+// }
 //
 ////==============================================================================
-//TEST_CASE("day18")
+// TEST_CASE("day18")
 //{
 //	// CHECK(sb::run_day_with_test_input(18, 1, 1) == "");
 //	// CHECK(sb::run_day_with_default_input(18, 1) == "");
 //
 //	// CHECK(sb::run_day_with_test_input(18, 2, 1) == "");
 //	// CHECK(sb::run_day_with_default_input(18, 2) == "");
-//}
+// }
 //
 ////==============================================================================
-//TEST_CASE("day19")
+// TEST_CASE("day19")
 //{
 //	// CHECK(sb::run_day_with_test_input(19, 1, 1) == "");
 //	// CHECK(sb::run_day_with_default_input(19, 1) == "");
 //
 //	// CHECK(sb::run_day_with_test_input(19, 2, 1) == "");
 //	// CHECK(sb::run_day_with_default_input(19, 2) == "");
-//}
+// }
 //
 ////==============================================================================
-//TEST_CASE("day20")
+// TEST_CASE("day20")
 //{
 //	// CHECK(sb::run_day_with_test_input(20, 1, 1) == "");
 //	// CHECK(sb::run_day_with_default_input(20, 1) == "");
 //
 //	// CHECK(sb::run_day_with_test_input(20, 2, 1) == "");
 //	// CHECK(sb::run_day_with_default_input(20, 2) == "");
-//}
+// }
 //
 ////==============================================================================
-//TEST_CASE("day21")
+// TEST_CASE("day21")
 //{
 //	// CHECK(sb::run_day_with_test_input(21, 1, 1) == "");
 //	// CHECK(sb::run_day_with_default_input(21, 1) == "");
 //
 //	// CHECK(sb::run_day_with_test_input(21, 2, 1) == "");
 //	// CHECK(sb::run_day_with_default_input(21, 2) == "");
-//}
+// }
 //
 ////==============================================================================
-//TEST_CASE("day22")
+// TEST_CASE("day22")
 //{
 //	// CHECK(sb::run_day_with_test_input(22, 1, 1) == "");
 //	// CHECK(sb::run_day_with_default_input(22, 1) == "");
 //
 //	// CHECK(sb::run_day_with_test_input(22, 2, 1) == "");
 //	// CHECK(sb::run_day_with_default_input(22, 2) == "");
-//}
+// }
 //
 ////==============================================================================
-//TEST_CASE("day23")
+// TEST_CASE("day23")
 //{
 //	// CHECK(sb::run_day_with_test_input(23, 1, 1) == "");
 //	// CHECK(sb::run_day_with_default_input(23, 1) == "");
 //
 //	// CHECK(sb::run_day_with_test_input(23, 2, 1) == "");
 //	// CHECK(sb::run_day_with_default_input(23, 2) == "");
-//}
+// }
 //
 ////==============================================================================
-//TEST_CASE("day24")
+// TEST_CASE("day24")
 //{
 //	// CHECK(sb::run_day_with_test_input(24, 1, 1) == "");
 //	// CHECK(sb::run_day_with_default_input(24, 1) == "");
 //
 //	// CHECK(sb::run_day_with_test_input(24, 2, 1) == "");
 //	// CHECK(sb::run_day_with_default_input(24, 2) == "");
-//}
+// }
 //
 ////==============================================================================
-//TEST_CASE("day25")
+// TEST_CASE("day25")
 //{
 //	// CHECK(sb::run_day_with_test_input(25, 1, 1) == "");
 //	// CHECK(sb::run_day_with_default_input(25, 1) == "");
 //
 //	// CHECK(sb::run_day_with_test_input(25, 2, 1) == "");
 //	// CHECK(sb::run_day_with_default_input(25, 2) == "");
-//}
+// }
 
 //==============================================================================
 #ifdef NDEBUG
